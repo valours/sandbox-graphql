93c51143d5543d14cf183a92214ccfe275956a3a
valours <valentin@fizix.io>
2018-11-14 14:33:43 +0100

    init api

---
+++ api/dataGenerator.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+// TODO: générer des utilisateurs, ajouter en parametres le nombres de user
+const userGenerator = () => {
+  console.log('userGenerator');
+}
+
+userGenerator();
---
+++ api/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+// TODO: Mettre à jour le schema avec des utilisateurs
+const schema = buildSchema(`
+  type Query {
+    random: Float!
+    hello: String
+    user: User
+  }
+
+  type User {
+    name: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+  random: () => {
+    return Math.random();
+  },
+  user: () => ({
+    name: 'damn',
+  })
+  //TODO: ajouter la méthod pour renvoyer les utilisateurs
+};
+
+const app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
